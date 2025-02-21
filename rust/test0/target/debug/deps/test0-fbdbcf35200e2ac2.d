3b18c5f833002975f2503cb1296e9b115b4dd95f
damidorg <andigo1316@gmail.com>
2025-02-21 14:32:14 +0200

    new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/main.rs 	new file:   target/.rustc_info.json 	new file:   target/CACHEDIR.TAG 	new file:   target/debug/.cargo-lock 	new file:   target/debug/.fingerprint/test0-fbdbcf35200e2ac2/bin-test0 	new file:   target/debug/.fingerprint/test0-fbdbcf35200e2ac2/bin-test0.json 	new file:   target/debug/.fingerprint/test0-fbdbcf35200e2ac2/dep-bin-test0 	new file:   target/debug/.fingerprint/test0-fbdbcf35200e2ac2/invoked.timestamp 	new file:   target/debug/deps/test0-fbdbcf35200e2ac2 	new file:   target/debug/deps/test0-fbdbcf35200e2ac2.d 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/2ry34p4bh0cjptw2wkla0n9bf.o 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/5b6uq03yssen17xt62756hxv8.o 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/5ekux6ndqf129kjawt4tyqwti.o 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/5sycar00simptenzq2shewkrb.o 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/ct3iljv2ywdgdu1shyfyh5xoj.o 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/dep-graph.bin 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/f01g0f4t9hlv0ydu924sps5b4.o 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/query-cache.bin 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7-603wolmqc456ojzohh8nqc3lb/work-products.bin 	new file:   target/debug/incremental/test0-1u131isgox1ll/s-h4ttrz9qm9-1as6hy7.lock 	new file:   target/debug/test0 	new file:   target/debug/test0.d

---
+++ rust/test0/target/debug/deps/test0-fbdbcf35200e2ac2.d
@@ -0,0 +1,5 @@
+/home/d20/programing/rust/test0/target/debug/deps/test0-fbdbcf35200e2ac2: src/main.rs
+
+/home/d20/programing/rust/test0/target/debug/deps/test0-fbdbcf35200e2ac2.d: src/main.rs
+
+src/main.rs:
---
+++ rust/test0/target/debug/test0.d
@@ -0,0 +1,1 @@
+/home/d20/programing/rust/test0/target/debug/test0: /home/d20/programing/rust/test0/src/main.rs
